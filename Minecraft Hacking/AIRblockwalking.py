d7cb428d9a03ae10e4c1c91b6313a267b7719c0a
Malachy <mdaniels7311@sohamcollege.org.uk>
2014-12-18 20:40:58 +0000

    Update AIRblockwalking.py

--- Minecraft Hacking/AIRblockwalking.py
+++ Minecraft Hacking/AIRblockwalking.py
@@ -3,13 +3,13 @@ import time
 mc = minecraft.Minecraft.create()
 
 while True:
-        pos = mc.player.getPos()
+        pos = mc.player.getPos() #gets the player co-ords
         x = pos.x
         y = pos.y
         z = pos.z
 
         Block = 0
 
-        mc.setBlock(x,y-1,z,Block)
+        mc.setBlock(x,y-1,z,Block) #sets the block beneath the player to the value of 0 (air)
         time.sleep(0.2)
 
