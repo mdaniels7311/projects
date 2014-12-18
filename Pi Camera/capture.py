5721067851ccd0687552c5bdc3fae30469bd7ab2
Malachy <mdaniels7311@sohamcollege.org.uk>
2014-12-18 20:41:55 +0000

    Update capture.py

--- Pi Camera/capture.py
+++ Pi Camera/capture.py
@@ -1,8 +1,8 @@
 import picamera, time
 
 with picamera.PiCamera() as cam:
-    cam.resolution = (1024,768)
-    cam.start_preview()
+    cam.resolution = (1024,768) #sets the camera resolution
+    cam.start_preview() #starts the camera view
 
-    time.sleep(2)
+    time.sleep(2) #waits 2 seconds then takes picture
     cam.capture('test.jpeg')
