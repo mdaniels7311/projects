9d44f36bfabb2e06db802f7a532226fffdd7a028
mdaniels7311 <mdaniels7311@sohamcollege.org.uk>
2015-01-27 12:12:21 +0000

    27/1/15

--- Ballon Popping Project/popping.py
+++ Ballon Popping Project/popping.py
@@ -1,6 +1,22 @@
-import RPi.GPIO as gp
+import RPi.GPIO as gp, time, picamera
 
-gp.setmode(gp.BCM)
-gp.setwarnings(False)
 button = 14
-gp.setup(button, gp.IN, gp.PUD_UP)
+ballon = 2
+
+with picamera.PiCamera() as camera:
+    gp.setmode(gp.BCM)
+    gp.setwarnings(False)
+    gp.setup(button, gp.IN, gp.PUD_UP)
+    gp.setup(ballon, gp.OUT)
+    print("Ready...")
+    gp.wait_for_edge(button, gp.FALLING)
+    gp.output(ballon, True)
+    time.sleep(5)
+    gp.output(ballon, False)
+    camera.resolution = (640, 480)
+    camera.framerate = 90
+    camera.start_preview()
+    camera.start_recording('ballon.h264')
+    camera.wait_recording(30)
+    camera.stop_recording()
+    camera.stop_preview()
